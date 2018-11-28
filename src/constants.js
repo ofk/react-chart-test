a902d9afe715d81b22f69a141ffd6213a43af965
ofk <ofkjpn+github@gmail.com>
2018-11-28 12:04:35 +0900

    Fix constants

--- src/constants.js
+++ src/constants.js
@@ -3,13 +3,15 @@ for (let y = 0; y < 50; y += 1) {
   yAxes.push(`y${y}`);
 }
 
-export const smallData = [];
+export const yAxesMini = yAxes.slice(0, 10);
+
+export const dataMini = [];
 for (let x = 0, xz = 10; x < xz; x += 1) {
   const d = { x };
   yAxes.forEach((yAxis, y) => {
     d[yAxis] = Math.sin(2 * Math.PI / xz * x) + Math.cos(x * y) / (yAxes.length - y + 1) / 10;
   });
-  smallData.push(d);
+  dataMini.push(d);
 }
 
 export const data = [];
